5f113e21e99dfec201c7bc5d2a01581c316f37ba
dharmik <dharmikpanchani96@gmail.com>
2023-01-11 14:03:53 +0530

    update img component

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from "react-redux";
-import Store from './redux/store/store'
+import store from './redux/store/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.Fragment>
     <BrowserRouter>
-      <Provider store={Store}>
+      <Provider store={store}>
         <App />
       </Provider>
     </BrowserRouter>
